251fde2be12376471a257f2bba486c1b6eea8c3c
Anton Matosov <anton.matosov@gmail.com>
2017-11-03 00:06:09 -0700

    Switch to standard JS Style instead of ESLint

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import Button from 'react-native-button';
+import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import Button from 'react-native-button'
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <View style={styles.stopwatchWrap}>
@@ -11,23 +11,23 @@ export default class App extends React.Component {
         </View>
         <View style={styles.buttonsContainer}>
           <Button style={styles.buttonLap}
-            disabled={true}>Lap</Button>
+            disabled={this.props.isRunning}>Lap</Button>
           <Button style={styles.buttonStart}>Start</Button>
         </View>
         <FlatList
-        style={styles.lapList}
-        data={[
-          {name: 'Lap 3', time: "00.02.23"},
-          {name: 'Lap 2', time: "00.05.23", style: "lapWorst"},
-          {name: 'Lap 1', time: "00.03.23", style: "lapBest"},
-        ]}
-        keyExtractor={ lap => lap.name }
-        renderItem={({item}) => 
-        <View style={styles.lap}>
-          <Text style={[styles.lapName, styles[item.style]]}>{item.name}</Text>
-          <Text style={[styles.lapTime, styles[item.style]]}>{item.time}</Text>
-        </View>
-      }/>
+          style={styles.lapList}
+          data={[
+            { name: 'Lap 3', time: '00.02.23' },
+            { name: 'Lap 2', time: '00.05.23', style: 'lapWorst' },
+            { name: 'Lap 1', time: '00.03.23', style: 'lapBest' }
+          ]}
+          keyExtractor={lap => lap.name}
+          renderItem={({ item }) =>
+            <View style={styles.lap}>
+              <Text style={[styles.lapName, styles[item.style]]}>{item.name}</Text>
+              <Text style={[styles.lapTime, styles[item.style]]}>{item.time}</Text>
+            </View>
+          } />
       </View>
     );
   }
@@ -38,18 +38,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'stretch',
-    justifyContent: 'space-between',
+    justifyContent: 'space-between'
   },
   stopwatchWrap: {
     flexGrow: 2,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   stopwatch: {
     flex: 0,
-    fontFamily: "Menlo-Regular",
+    fontFamily: 'Menlo-Regular',
     fontSize: 69,
-    textAlign: "center",
+    textAlign: 'center',
     paddingTop: 60
   },
   lapList: {
@@ -60,20 +60,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginHorizontal: 20,
+    marginHorizontal: 20
   },
   lapName: {
     fontSize: 16
   },
   lapTime: {
-    fontFamily: "Menlo-Regular",
-    fontSize: 16,
+    fontFamily: 'Menlo-Regular',
+    fontSize: 16
   },
   lapWorst: {
-    color: "red"
+    color: 'red'
   },
   lapBest: {
-    color: "green"
+    color: 'green'
   },
   buttonsContainer: {
     flex: 1,
@@ -82,17 +82,17 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginHorizontal: 45,
-    marginVertical: 20,
+    marginVertical: 20
   },
   buttonLap: {
   },
   buttonReset: {
-    color: "red"
+    color: 'red'
   },
   buttonStart: {
-    color: "green"
+    color: 'green'
   },
   buttonStop: {
-    color: "red"
+    color: 'red'
   }
-});
+})
